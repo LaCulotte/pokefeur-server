02d85c4ceeeed62abdab32b0330532d74f561d90
Christophe <negeko-inscriptions@hotmail.com>
2025-12-27 16:06:19 +0100

    fix(scroll): Consistantly hide scrollbar on chrome desktop

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,3 +1,17 @@
 <template>
     <RouterView />
 </template>
+
+<style lang="css">
+.hide-scroll {
+    flex: 1 1 auto;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+}
+
+.hide-scroll::-webkit-scrollbar {
+  display: none;
+}
+</style>
