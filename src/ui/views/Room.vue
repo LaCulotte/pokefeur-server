e3b103b03259c39212bd2ef911c5e4f87c075bb6
Christophe <negeko-inscriptions@hotmail.com>
2025-12-27 16:05:44 +0100

    feat(room): Better room layout

--- src/ui/views/Room.vue
+++ src/ui/views/Room.vue
@@ -11,23 +11,32 @@ function goToCollection() {
 
 <template>
     <div class="background h-screen">
-        <v-img ref="img" class="main-img" style="" src="/static/images/Room-v0.png"></v-img>
+        <div class="h-100 d-flex flex-column justify-center">
+            <div style="flex: 1 1" class="d-flex flex-column justify-center">
+                <div class="pa-6">
+                    <v-progress-linear
+                    color="light-green-darken-4"
+                    height="30"
+                    model-value="50"
+                    striped
+                    rounded="xl"
+                    style="box-shadow: 0 0 10px;"
+                    >
+                        <strong style="color: white">Booster en cours; 69 mins restantes ...</strong>
+                    </v-progress-linear>
+                </div>
+            </div>
 
-        <div class="position-absolute w-100 pa-6" style="top: 3%;">
-            <v-progress-linear
-            color="light-green-darken-4"
-            height="30"
-            model-value="50"
-            striped
-            rounded="xl"
-            style="box-shadow: 0 0 10px;"
-            >
-                <strong style="color: white">Booster en cours; 69 mins restantes ...</strong>
-            </v-progress-linear>
+            <div style="flex: 3 3; max-height: 90%">
+                <v-img class="main-img h-100" ref="img" src="/static/images/Room-v0.png"></v-img>
+            </div>
+
+            <div style="flex: 1 1"></div>
         </div>
 
+
         <div class="position-absolute d-flex" style="bottom: 5%; left: 5%;">
-            <v-btn class="collection-btn" rounded="xl" icon="$vuetify" @click="goToCollection"></v-btn>
+            <v-btn class="collection-btn" rounded="xl" icon="mdi-view-grid-outline" @click="goToCollection"></v-btn>
             <div class="flex-shrink-0" style="flex: 2 2;"></div>
         </div>
     </div>
@@ -45,9 +54,4 @@ function goToCollection() {
     left: 50%;
     transform: translate(-50%, -50%);
 }
-
-.filler {
-    flex: 1 1;
-}
-
 </style>
