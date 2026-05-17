146db3d2613f33feab9dfcacdc173d840886886b
Christophe <negeko-inscriptions@hotmail.com>
2026-05-17 19:23:23 +0200

    feat(lang): Adding the possibility to change language with the lang query parameter

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+import type { SupportedLanguages } from "../../resources/interfaces";
+import { LANGS } from "../common/constants";
+import { lang } from "./controller/lang";
+
+const urlLang = new URLSearchParams(window.location.search).get("lang");
+if (urlLang && LANGS.includes(urlLang as SupportedLanguages)) {
+    lang.value = urlLang as SupportedLanguages;
+}
+</script>
+
 <template>
     <router-view />
 </template>
